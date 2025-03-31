5f2ff92c83d2c6e48a85c501af24056e0e6a9c43
ZacWJ1 <142571679+ZacWJ1@users.noreply.github.com>
2025-03-30 18:28:08 -0700

    removed paper

--- public/src/pages/signup.js
+++ public/src/pages/signup.js
@@ -26,14 +26,14 @@ function SignUp(){
                 }
             });
     };
-   // const paperStyle = { padding: "2rem", margin: "100px auto" };
+    const paperStyle = { padding: "2rem", margin: "100px auto" };
     const heading = { fontSize: "2.5rem", fontWeight: "600" };
     const row = { display: "flex", marginTop: "2rem" };
     const btnStyle = { marginTop: "2rem", fontSize: "1.2rem", fontWeight: "700", backgroundColor: "blue", borderRadius: "0.5rem" };
     return (
         <div>
-            <Grid2 align="center" >
-                            <Paper  >
+            <Grid2 align="center" className="wrapper">
+                            
                                 <Typography component="h1" variant="h5" style={heading}>Login</Typography>
                     <form onSubmit={handleSignup}>
                         <TextField style={row} sx={{label: { fontWeight: '700', fontSize:"1.3rem" }}} fullWidth type="text" label="Enter Name" name="name" onChange={(e)=>setName(e.target.value)}></TextField>
@@ -42,7 +42,7 @@ function SignUp(){
                         <Button style={btnStyle} variant="contained" type="submit">SignUp</Button>
                     </form>
                     <p>Already have an account?<Link href="/login"> Login</Link></p>
-                </Paper>
+                
             </Grid2>
         </div>
     )
